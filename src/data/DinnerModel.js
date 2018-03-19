6ebd2fe14ce49ed96e9dfa787b84de32df4c3763
Jordan Rye <jordanrye1@gmail.com>
2018-03-19 01:26:15 +0100

    Re-implemented model functions

--- src/data/DinnerModel.js
+++ src/data/DinnerModel.js
@@ -4,28 +4,100 @@ const httpOptions = {
 
 const DinnerModel = function () {
   let numberOfGuests = 4;
-  let observers = [];
+  var selectedDishes = [];
+  var observers = [];
 
-  // Sets the number of guests. The number of guests cannot go below 1.
-  this.setNumberOfGuests = function (num) {
+  // Conditions
+  this.GUESTS = "guests";
+  this.DISH = "dish";
+
+  // Error messages
+  this.ERROR_SUFFIX = "Something went wrong...";
+  this.ERROR_PRICE_OF_DISH = "Cannot get price of dish.";
+  this.ERROR_ADD_DISH = "Cannot add dish to menu.";
+  this.WEB_ERROR_MESSAGE = "<div class=\"row equal\" style=\"text-align:center;\">Something went wrong, please try again in a few seconds...</div>";
+
+  // Sets the number of guests.
+  this.setNumberOfGuests = (num) => {
     numberOfGuests = num;
-    notifyObservers();
-  };
+    notifyObservers(this.GUESTS);
+  }
 
   // Retrieve the number of guests.
   this.getNumberOfGuests = () => numberOfGuests;
 
-  // API Calls
+  // Returns the dish that is on the menu for selected type
+  this.getSelectedDish = (type) => {
+    for (let dish in selectedDishes) {
+      if (dish.dishTypes[0] === type) {
+        return dish;
+      }
+    }
+  }
 
-  this.getAllDishes = function () {
-    const url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search'
+  // Returns all the dishes on the menu.
+  this.getFullMenu = () => selectedDishes;
+
+  // Returns all ingredients for all the dishes on the menu.
+  this.getAllIngredients = () => {
+    var ingredients = [];
+    for (let i = 0; i < selectedDishes.length; i++) {
+      for (let j = 0; j < selectedDishes[i].ingredients.length; j++) {
+        ingredients.push(selectedDishes[i].ingredients[j]);
+      }
+    }
+    return ingredients;
+  }
+
+  // Returns the total price of the menu (all the ingredients multiplied by
+  // number of guests) rounded to 2 decimal places.
+  this.getTotalMenuPrice = () => Math.round(selectedDishes.reduce((price, dish) => price + this.getPriceOfDish(dish), 0) * 100) / 100;
+
+  // Returns the price of a dish rounded to 2 decimal places.
+  this.getPriceOfDish = (dish) => Math.round(dish.pricePerServing * dish.servings * numberOfGuests * 100) / 100;
+
+  // Adds the passed dish to the menu. If the dish of that type already exists
+  // on the menu it is removed from the menu and the new one added.
+  this.addDishToMenu = (id) => {
+    this.getDish(id, (dish) => {
+      selectedDishes.push(dish);
+      notifyObservers(this.DISH);
+    }, (error) => {
+      console.log(generateErrorMessage(this.ERROR_ADD_DISH, this.ERROR_SUFFIX));
+    });
+  }
+
+  // Removes dish from menu
+  this.removeDishFromMenu = (id) => {
+    for (let idx = 0; idx < selectedDishes.length; ++idx) {
+      if (selectedDishes[idx].id === id) {
+        selectedDishes.splice(idx, 1);
+        notifyObservers(this.DISH);
+        return;
+      }
+    }
+  }
+
+  // Function that returns all dishes of specific type (i.e. "starter", "main
+  // dish", "dessert"). You can use the filter argument to filter out the dish
+  // by name or ingredient (use for search). If you don't pass any filter all
+  // the dishes will be returned.
+  this.getAllDishes = function (type='', filter='') {
+    const url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?type=' + type + '&query=' + filter + '&number=16';
     return fetch(url, httpOptions)
       .then(processResponse)
       .catch(handleError)
   }
 
-  // API Helper methods
+  // Function that returns a dish of specific ID
+  this.getDish = (id) => {
+    const url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/' + id + '/information';
+    return fetch(url, httpOptions)
+      .then(processResponse)
+      .catch(handleError)
+  }
 
+  // Helper function to process API responses.
   const processResponse = function (response) {
     if (response.ok) {
       return response.json()
@@ -33,6 +105,7 @@ const DinnerModel = function () {
     throw response;
   }
 
+  // Helper function to handle errors on failed API calls.
   const handleError = function (error) {
     if (error.json) {
       error.json().then(error => {
@@ -43,18 +116,20 @@ const DinnerModel = function () {
     }
   }
 
-  // Observer pattern
+  // Helper function to generate error messages on failed AJAX calls.
+  var generateErrorMessage = (prefix, suffix) => prefix + " " + suffix;
 
-  this.addObserver = function (observer) {
-    observers.push(observer);
+  // Add observer to list of observers.
+  this.addObserver = (observer) => observers.push(observer);
+
+  // Find and remove observer from list of observers.
+  this.removeObserver = (observer) => {
+    observers = observers.filter(obs => obs !== observer);
   };
 
-  this.removeObserver = function (observer) {
-    observers = observers.filter(o => o !== observer);
-  };
-
-  const notifyObservers = function () {
-    observers.forEach(o => o.update());
+  // Notify all observers that they need to be updated.
+  const notifyObservers = function (condition) {
+    observers.forEach(obs => obs.update(condition));
   };
 };
 
